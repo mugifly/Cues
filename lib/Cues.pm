93c9fc47b7edbad3ec833198c3d57d939cd3f93c
Masanori Ohgita <mp@ohgita.info>
2012-09-14 19:47:50 +0900

    replace controller namespace

--- lib/Cues.pm
+++ lib/Cues.pm
@@ -3,16 +3,19 @@ use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
-
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
-
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+	my $self = shift;
+	
+	# Documentation browser under "/perldoc"
+	$self->plugin('PODRenderer');
+	
+	# Router
+	my $r = $self->routes;
+	
+	# Set namespace
+	$r->namespace('Cues::Controller');
+	
+	# Normal route to controller
+	$r->get('/')->to('top#welcome');
 }
 
 1;
---
+++ lib/Cues/Controller/Top.pm
@@ -0,0 +1,11 @@
+package Cues::Controller::Top;
+use Mojo::Base 'Mojolicious::Controller';
+
+sub welcome {
+	my $self = shift;
+	
+	$self->render(
+		message => 'Welcome to the Mojolicious real-time web framework!');
+}
+
+1;
